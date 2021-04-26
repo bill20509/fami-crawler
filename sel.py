c87f256738dcbd8c49bcb16e8f0ba453a675e120
bill <maxbill20509@gmail.com>
2021-04-27 05:46:40 +0800

    update:successfuly craw the source from website

--- sel.py
+++ sel.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 driver = webdriver.Chrome()
 driver.get("https://foodsafety.family.com.tw/Web_FFD/Page/FFD_1_1.aspx#header") 
 button = driver.find_element_by_name("ctl00$ContentPlaceHolder1$ctl00")
@@ -12,6 +13,16 @@ try:
     )
 except:
     driver.quit()
-btn = driver.find_element_by_xpath("/html[1]/body[1]/form[1]/div[3]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/a[1]")
-btn.click()
+source = driver.page_source
+soup = BeautifulSoup(source, 'html.parser')
+f = open("source.txt", "w", encoding='utf-8')
+f.write(source)
+f.close()
+for a in soup.find_all('a', href=True):
+    print ("Found the URL:", "+", a['href'])
+    driver.get("https://foodsafety.family.com.tw/Web_FFD/Page/" + a['href']) #https://foodsafety.family.com.tw/Web_FFD/Page/ + 
+    item_source = driver.page_source
+    item_soup = BeautifulSoup(item_source, "html.parser")
+    for h in item_soup.find_all('h5'):
+        print (h.text)
 
