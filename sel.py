bd7a2b4507f76a2dd1538ba899244bb4f77dc207
bill <maxbill20509@gmail.com>
2021-04-27 04:47:44 +0800

    update:access to second website

--- sel.py
+++ sel.py
@@ -6,9 +6,12 @@ driver = webdriver.Chrome()
 driver.get("https://foodsafety.family.com.tw/Web_FFD/Page/FFD_1_1.aspx#header") 
 button = driver.find_element_by_name("ctl00$ContentPlaceHolder1$ctl00")
 button.click()
-# try:
-#     element = WebDriverWait(driver, 100).until(
-#         EC.presence_of_element_located((By.CSS_SELECTOR, ".popup-with-zoom-anim"))
-#     )
-# finally:
-#     driver.quit()
+try:
+    element = WebDriverWait(driver, 5000).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "bann-strip"))
+    )
+except:
+    driver.quit()
+btn = driver.find_element_by_xpath("/html[1]/body[1]/form[1]/div[3]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/a[1]")
+btn.click()
+
