82f6785919f850f162f7f25db789f89bb12cdf8c
bill <maxbill20509@gmail.com>
2021-04-28 05:09:22 +0800

    update: transfer date into json

--- crawl.py
+++ crawl.py
@@ -1,6 +1,24 @@
 import requests
+import json 
 from bs4 import BeautifulSoup
+import re
 response = requests.get(
     "https://foodsafety.family.com.tw/Web_FFD/Page/FFD_1_2.aspx?CatNa=2F8BF38E932812A29D50A09490280B038A150772E67BA540&CatNo=C018977062F3E7E4&CmnoCode=08C2324880F88EDA&Img=003004.0612026.jpg")
 soup = BeautifulSoup(response.text, "html.parser")
-print(soup.prettify())  #輸出排版後的HTML內容
+item = {}
+for h in soup.find('h4'):
+    item['name'] = h[5:]
+i = 0
+allItems = {}
+
+attr = ['calories', 'protein', 'fat', 'carb', 'sodium', 'sugar', 'image']
+for h in soup.find_all('h5')[4:7]:
+    item[attr[i]] = float(re.findall("\d+\.?\d*", h.text)[0])
+    i = i + 1
+for h in soup.find_all('h5')[9:12]:
+    item[attr[i]] = float(re.findall("\d+\.?\d*", h.text)[0])
+    i = i + 1
+
+allItems[0] = item
+y = json.dumps(allItems, ensure_ascii=False)
+print(y)
